03abab5572d1f94d339037205612d902672bfefb
juliscapucin <jscapucin@gmail.com>
2025-04-07 21:08:35 +0100

    remove cards list

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@
 
 import { useEffect, useState } from "react";
 
-import { CardsList, Header } from "./components/";
+import { deleteIdea, saveIdea } from "./lib";
+
+import { Card, Header } from "./components/";
 
 import { IdeaCard, SortOption } from "./types";
 import { createIdea, saveToLocalStorage, sortIdeas } from "./lib";
+import { motion } from "motion/react";
 
 function App() {
     const [isFirstLoad, setIsFirstLoad] = useState(true);
@@ -18,6 +21,48 @@ function App() {
     const handleCreateIdea = () => {
         setIdeaCardCollection(createIdea(ideaCardCollection));
         handleSort(null);
+    };
+
+    useEffect(() => {
+        if (ideaCardCollection.length === 0) setSortChoice(null); // Clear sort menu choice if collection is empty
+    }, [setSortChoice, ideaCardCollection]);
+
+    // SAVE IDEA
+    const handleSaveIdea = (
+        card: IdeaCard,
+        newTitle: string,
+        newDescription: string
+    ):
+        | { status: "success" }
+        | { status: "error"; message?: string; previousTitle?: string } => {
+        setSortChoice(null); // Reset sort menu
+
+        const result = saveIdea({
+            card,
+            newTitle,
+            newDescription,
+            ideaCardCollection,
+        });
+
+        if (result.status === "error") {
+            return {
+                status: "error",
+                message: result.message,
+                previousTitle: card.title,
+            };
+        }
+
+        // Update collection
+        setIdeaCardCollection(result.updatedCollection);
+
+        return { status: "success" };
+    };
+
+    // DELETE IDEA
+    const handleDeleteIdea = (id: string) => {
+        const updatedCollection = deleteIdea(id, ideaCardCollection);
+
+        setIdeaCardCollection(updatedCollection);
     };
 
     // SORT IDEAS
@@ -53,11 +98,35 @@ function App() {
                 sortChoice={sortChoice}
             />
             <main className='main'>
-                <CardsList
-                    ideaCardCollection={ideaCardCollection}
-                    setIdeaCardCollection={setIdeaCardCollection}
-                    setSortChoice={setSortChoice}
-                />
+                <motion.div
+                    className='cards-list__container'
+                    layout // Motion settings
+                    transition={{ duration: 0.2 }}
+                    id='cards-list-container'
+                >
+                    {ideaCardCollection.length === 0 ? (
+                        <div className='cards-list__no-cards'>
+                            <p>No ideas in this collection</p>
+                        </div>
+                    ) : (
+                        ideaCardCollection.map((card) => {
+                            return (
+                                <Card
+                                    key={`ideaCard-${card.id}`}
+                                    ideaCard={card}
+                                    onSave={(newTitle, newDescription) =>
+                                        handleSaveIdea(
+                                            card,
+                                            newTitle,
+                                            newDescription
+                                        )
+                                    }
+                                    onDelete={() => handleDeleteIdea(card.id)}
+                                />
+                            );
+                        })
+                    )}
+                </motion.div>
             </main>
         </>
     );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,19 +1,17 @@
-import Alert from "./Alert"
-import Card from "./Card"
-import CardsList from "./CardsList"
-import CharacterCountdown from "./CharacterCountdown"
-import Header from "./Header"
-import InstructionsPopup from "./InstructionsPopup"
-import SortMenu from "./SortMenu"
-import Toast from "./Toast"
+import Alert from "./Alert";
+import Card from "./Card";
+import CharacterCountdown from "./CharacterCountdown";
+import Header from "./Header";
+import InstructionsPopup from "./InstructionsPopup";
+import SortMenu from "./SortMenu";
+import Toast from "./Toast";
 
 export {
-	Alert,
-	Card,
-	CardsList,
-	CharacterCountdown,
-	Header,
-	InstructionsPopup,
-	SortMenu,
-	Toast,
-}
+    Alert,
+    Card,
+    CharacterCountdown,
+    Header,
+    InstructionsPopup,
+    SortMenu,
+    Toast,
+};
